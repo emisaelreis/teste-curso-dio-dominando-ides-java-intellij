7b0d57c312fca0746566054a53f24d1eaaabdbc6
emisaelreis <emisaelcavalcante@gmail.com>
2023-03-14 17:33:16 -0300

    second commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,10 +4,22 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World " + (a+b));*/
+        Gato gato = new Gato();
+        Livro livro = new Livro("O cortiço", 240);
+
         System.out.println(gato);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer nPag;
+
+    Livro(String nome, Integer nPag) {
+        this.nome = nome;
+        this.nPag = nPag;
     }
 }
